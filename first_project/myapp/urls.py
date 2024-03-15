c56feedc0c388d55e7ca81518f2a9ddbdd9518b5
MutaiAllan <allankiprop175@gmail.com>
2024-03-15 12:50:20 +0300

    Added url param

--- first_project/myapp/urls.py
+++ first_project/myapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("", views.index, name='index')
+    path("", views.index, name='index'),
+    path("about", views.index,name='index'),
+    path("time", views.current_datetime, name='current_datetime'),
+    path("<str:name>", views.greet, name='greet')
 ]
--- first_project/myapp/views.py
+++ first_project/myapp/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello world!")
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+def greet(request, name):
+    return HttpResponse(f"Hello {name}!")
